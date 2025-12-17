d51b7fc93682b55dbfa01e3cb0429b84727b0bca
ritams <ritam4jnu@gmail.com>
2025-12-17 13:40:15 +0530

    feat: Add PM2 configuration for the backend application.

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: "manage-backend",
+            script: "./server.js",
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: "1G",
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            },
+            error_file: "logs/err.log",
+            out_file: "logs/out.log",
+            time: true,
+        },
+    ],
+};
